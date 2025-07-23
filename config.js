c3c10a588194df0bc48ab94fb932c12f8086a299
itxabrham07 <itxabraham07@gmail.com>
2025-07-23 14:40:54 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,30 @@
+export const config = {
+  instagram: {
+    username: 'itxrey', // Your Instagram username
+    password: 'your_instagram_password', // Your Instagram password
+    useMongoSession: true // Set to false to use file-based sessions
+  },
+  
+  telegram: {
+    botToken: '7580382614:AAH30PW6TFmgRzbC7HUXIHQ35GpndbJOIEI',
+    chatId: '-1002710686896',
+    enabled: true,
+    forwardMessages: true,
+    forwardMedia: true
+  },
+  
+  mongo: {
+    uri: 'mongodb+srv://itxelijah07:ivp8FYGsbVfjQOkj@cluster0.wh25x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    dbName: 'hyper_insta',
+  },
+
+  
+  admin: {
+    users: ['itxrey', 'iarshman'] // Admin usernames
+  },
+  
+  app: {
+    logLevel: 'info',
+    environment: 'development'
+  }
+};
